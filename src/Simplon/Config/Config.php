5290605b8fdaea4d88f7162fd861c56f823143b7
Tino Ehrich <ehrich@efides.com>
2012-10-18 17:48:58 +0200

    init

---
+++ src/Simplon/Config/Config.php
@@ -0,0 +1,83 @@
+<?php
+
+  namespace Simplon\Config;
+
+  class Config
+  {
+    private $_configPath;
+
+    /**
+     * @var array
+     */
+    protected $_config = array();
+
+    // ########################################
+
+    public function setConfigPath($path)
+    {
+      $this->_configPath = $path;
+      return $this;
+    }
+
+    // ########################################
+
+    public function getConfigPath()
+    {
+      return $this->_configPath;
+    }
+
+    // ########################################
+
+    public function getConfig()
+    {
+      if(! $this->_config)
+      {
+        $app = array();
+
+        require $this->getConfigPath();
+
+        /**
+         * get current environment
+         */
+        $env = $app['environment'];
+
+        /**
+         * insert appName in environment
+         */
+        $app[$env]['appName'] = $app['appName'];
+
+        /**
+         * only enabled environment
+         */
+        $this->_config = $app[$env];
+      }
+
+      return $this->_config;
+    }
+
+    // ########################################
+
+    /**
+     * @param array $keys
+     * @return mixed
+     * @throws \Exception
+     */
+    public function getConfigByKeys(array $keys)
+    {
+      $config = $this->getConfig();
+
+      foreach($keys as $key)
+      {
+        if(array_key_exists($key, $config))
+        {
+          $config = $config[$key];
+        }
+        else
+        {
+          throw new \Exception('Config key "' . implode('->', $keys) . '" doesnt exist.');
+        }
+      }
+
+      return $config;
+    }
+  }
